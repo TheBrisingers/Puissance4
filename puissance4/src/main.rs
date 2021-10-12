1cb6d4d37163afb146ec09f223fdeff152777b95
TheBrisingers <timothee.jouffrieau@gmail.com>
2021-10-12 11:25:17 +0200

    add reset

--- puissance4/src/main.rs
+++ puissance4/src/main.rs
@@ -8,7 +8,21 @@ use piston_window::*;
 
 fn main() {
     let opengl = OpenGL::V3_2;
-    let (width, height) = (1000, 800);
+    
+    let size_tile = 100.0;
+    const NB_COLUMN :i64 = 7;
+    const NB_RAW :i64 = 6;
+    let margin_x = 120.0;
+    let margin_y = 120.0;
+    let blue = [0.0, 0.0, 1.0, 1.0];
+    let size_grille = math::margin_rectangle([margin_x, margin_y, size_tile*NB_COLUMN as f64, size_tile*NB_RAW as f64], 0.0);
+    let mut cursor_column = 0;
+    let mut game_finish = false;
+
+    let mut current_player = Token::YELLOW;
+
+    let (width, height) = ((size_tile*(NB_COLUMN as f64)+ margin_x*2.0) as u32, (size_tile*(NB_RAW as f64)+ margin_y*2.0) as u32);
+
     let mut window: PistonWindow = WindowSettings::new("shapes", (width, height))
         .exit_on_esc(true)
         .graphics_api(opengl)
@@ -16,27 +30,16 @@ fn main() {
         .unwrap();
     window.set_lazy(true);
 
-    let size_tile = 100.0;
-    const NB_COLUMN :i64 = 7;
-    const NB_RAW :i64 = 6;
-    let margin_x = 20.0;
-    let margin_y = 120.0;
-    let blue = [0.0, 0.0, 1.0, 1.0];
-    let size_grille = math::margin_rectangle([margin_x, margin_y, size_tile*NB_COLUMN as f64, size_tile*NB_RAW as f64], 0.0);
-    let mut cursor_column = 0;
-    let mut game_finish = false;
-
-    let mut current_player = Jeton::YELLOW;
 
     #[derive(Copy,Clone)]
-    enum Jeton {
+    enum Token {
         NONE =0,
         YELLOW,
         RED
     }
 
 
-    let mut grid = [[Jeton::NONE;NB_RAW as usize];NB_COLUMN as usize];
+    let mut grid = [[Token::NONE;NB_RAW as usize];NB_COLUMN as usize];
 
 
 
@@ -53,16 +56,16 @@ fn main() {
                     let mut count = NB_RAW-1 as i64;
                     while count>=0{
                         match grid[cursor_column as usize][count as usize] {
-                            Jeton::NONE => {grid[cursor_column as usize][count as usize] = current_player;
+                            Token::NONE => {grid[cursor_column as usize][count as usize] = current_player;
                                 match current_player {
                                     //send to socket
-                                    Jeton::RED => {current_player = Jeton::YELLOW}
-                                    Jeton::YELLOW => {current_player = Jeton::RED}
-                                    Jeton::NONE => {}
+                                    Token::RED => {current_player = Token::YELLOW}
+                                    Token::YELLOW => {current_player = Token::RED}
+                                    Token::NONE => {}
                                 }
                                 break;
                             }
-                            Jeton::YELLOW | Jeton::RED => {count-=1}
+                            Token::YELLOW | Token::RED => {count-=1}
                         }
                     }
                     //verif columns
@@ -73,20 +76,13 @@ fn main() {
                         count_yellow = 0 as i64;
                         for cell in col.iter() {
                             match cell  {
-                                Jeton::NONE => {count_yellow = 0; count_red = 0},
-                                Jeton::YELLOW => {count_red = 0; count_yellow +=1},
-                                Jeton::RED => {count_yellow = 0; count_red +=1}
+                                Token::NONE => {count_yellow = 0; count_red = 0},
+                                Token::YELLOW => {count_red = 0; count_yellow +=1},
+                                Token::RED => {count_yellow = 0; count_red +=1}
                             }
-                            if count_red == 4 {
-                                println!("RED WIN column !!");
-                                game_finish =true;
-                                break;
-                            }
-                            else if count_yellow == 4 {
-                                println!("YELLOW WIN column !!");
-                                game_finish =true;
-                                break;
-                            } 
+                            if check_winner(count_yellow, count_red){
+                                game_finish = true
+                            };
                             
                         }
                     } 
@@ -97,20 +93,13 @@ fn main() {
                         for col in 0..NB_COLUMN {
                             let cell = grid[col as usize][raw as usize];
                             match cell  {
-                                Jeton::NONE => {count_yellow = 0; count_red = 0},
-                                Jeton::YELLOW => {count_red = 0; count_yellow +=1},
-                                Jeton::RED => {count_yellow = 0; count_red +=1}
+                                Token::NONE => {count_yellow = 0; count_red = 0},
+                                Token::YELLOW => {count_red = 0; count_yellow +=1},
+                                Token::RED => {count_yellow = 0; count_red +=1}
                             }
-                            if count_red == 4 {
-                                println!("RED WIN raw !!");
-                                game_finish =true;
-                                break;
-                            }
-                            else if count_yellow == 4 {
-                                println!("YELLOW WIN raw !!");
-                                game_finish =true;
-                                break;
-                            }
+                            if check_winner(count_yellow, count_red){
+                                game_finish = true
+                            };
                         }
                     }   
 
@@ -123,20 +112,13 @@ fn main() {
                             if col<NB_COLUMN && raw<NB_RAW {
                                 let cell = grid[col as usize][raw as usize];
                                 match cell  {
-                                    Jeton::NONE => {count_yellow = 0; count_red = 0},
-                                    Jeton::YELLOW => {count_red = 0; count_yellow +=1},
-                                    Jeton::RED => {count_yellow = 0; count_red +=1}
+                                    Token::NONE => {count_yellow = 0; count_red = 0},
+                                    Token::YELLOW => {count_red = 0; count_yellow +=1},
+                                    Token::RED => {count_yellow = 0; count_red +=1}
                                 }
-                                if count_red == 4 {
-                                    println!("RED WIN diag 1!!");
-                                    game_finish =true;
-                                    break;
-                                }
-                                else if count_yellow == 4 {
-                                    println!("YELLOW WIN diag 1!!");
-                                    game_finish =true;
-                                    break;
-                                }
+                                if check_winner(count_yellow, count_red){
+                                    game_finish = true
+                                };
                             }
                         }
                     }  
@@ -149,19 +131,12 @@ fn main() {
                             if col<NB_COLUMN && raw<NB_RAW {
                                 let cell = grid[col as usize][NB_RAW as usize - raw as usize -1];
                                 match cell  {
-                                    Jeton::NONE => {count_yellow = 0; count_red = 0},
-                                    Jeton::YELLOW => {count_red = 0; count_yellow +=1},
-                                    Jeton::RED => {count_yellow = 0; count_red +=1}
+                                    Token::NONE => {count_yellow = 0; count_red = 0},
+                                    Token::YELLOW => {count_red = 0; count_yellow +=1},
+                                    Token::RED => {count_yellow = 0; count_red +=1}
                                 }
-                                if count_red == 4 {
-                                    println!("RED WIN diag 2!!");
-                                    game_finish =true;
-                                    break;
-                                }
-                                else if count_yellow == 4 {
-                                    println!("YELLOW WIN diag 2!!");
-                                    game_finish =true;
-                                    break;
+                                if check_winner(count_yellow, count_red){
+                                    game_finish = true
                                 }
                             }
                         }
@@ -170,7 +145,19 @@ fn main() {
                 }
             };
             //efface la preview si la parti est fini
-            if game_finish {current_player=Jeton::NONE}
+            if game_finish {current_player=Token::NONE}
+        }
+        if let Some(button) = e.press_args() {
+            use piston_window::Button::Keyboard;
+
+            if button == Keyboard(Key::R) {
+                grid = [[Token::NONE;NB_RAW as usize];NB_COLUMN as usize];
+                println!("Reset grid");
+                if game_finish{
+                    current_player = Token::YELLOW;
+                    game_finish = false
+                }
+            }
         }
         window.draw_2d(&e, |c, g, _| {
             clear([1.0; 4], g);
@@ -178,9 +165,9 @@ fn main() {
             let size_jeton= [margin_x + size_tile /10.0 + size_tile * cursor_column as f64, size_tile /10.0, size_tile * 8.0/10.0, size_tile * 8.0/10.0];
             let next_jeton_preview;
             match current_player {
-                Jeton::NONE => {next_jeton_preview = [1.0, 1.0, 1.0, 1.0]},
-                Jeton::YELLOW => {next_jeton_preview = [1.0, 0.8, 0.0, 1.0];},
-                Jeton::RED => {next_jeton_preview = [1.0, 0.0, 0.0, 1.0];}
+                Token::NONE => {next_jeton_preview = [1.0, 1.0, 1.0, 1.0]},
+                Token::YELLOW => {next_jeton_preview = [1.0, 0.8, 0.0, 1.0];},
+                Token::RED => {next_jeton_preview = [1.0, 0.0, 0.0, 1.0];}
             }
             ellipse(next_jeton_preview, size_jeton, c.transform, g);
             
@@ -192,9 +179,9 @@ fn main() {
                     let size_trou = [margin_x + size_tile /10.0 + size_tile * i as f64, margin_y + size_tile /10.0 + size_tile * j as f64, size_tile * 8.0/10.0, size_tile * 8.0/10.0];
                     let color_jeton;
                     match cell {
-                        Jeton::NONE => {color_jeton = [1.0, 1.0, 1.0, 1.0];},
-                        Jeton::YELLOW => {color_jeton = [1.0, 0.8, 0.0, 1.0];},
-                        Jeton::RED => {color_jeton = [1.0, 0.0, 0.0, 1.0];}
+                        Token::NONE => {color_jeton = [1.0, 1.0, 1.0, 1.0];},
+                        Token::YELLOW => {color_jeton = [1.0, 0.8, 0.0, 1.0];},
+                        Token::RED => {color_jeton = [1.0, 0.0, 0.0, 1.0];}
                     }
                     ellipse(color_jeton, size_trou, c.transform, g);
  
@@ -203,5 +190,21 @@ fn main() {
 
         });
         
+    }
+
+    fn check_winner(count_yellow : i64, count_red : i64)->bool{
+        if count_red == 4 {
+            println!("RED WIN diag 2!!");
+            //game_finish =true;
+            return true;
+        }
+        else if count_yellow == 4 {
+            println!("YELLOW WIN diag 2!!");
+            //game_finish =true;
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
