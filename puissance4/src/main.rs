7744f6a436d473b0c20580578b507ee9626b6a7f
TheBrisingers <timothee.jouffrieau@gmail.com>
2021-11-04 00:50:27 +0100

    change command select column

--- puissance4/src/main.rs
+++ puissance4/src/main.rs
@@ -17,11 +17,8 @@ fn main() {
     let margin_y = size_tile + 20.0;
     let color_blue = [0.0, 0.0, 1.0, 1.0];
     let size_grille = math::margin_rectangle([margin_x, margin_y, size_tile*NB_COLUMN as f64, size_tile*NB_RAW as f64], 0.0);
-    let mut cursor_column = 0;
-    let mut game_finish = false;
+    let mut game_finish : bool = false;
     let mut the_winner : Token = Token::NONE;
-
-    let mut my_turn = false;
 
     let color_white = [1.0,1.0,1.0,1.0];
     let color_yellow = [1.0,0.8,0.0,1.0];
@@ -84,81 +81,43 @@ fn main() {
 
 
     while let Some(e) = window.next() {
-        if !game_finish{
-            if let Some(pos) = e.mouse_cursor_args() {
-                cursor_column = ((pos[0] as f64-margin_x)/size_tile) as i64;
-                if cursor_column>=NB_COLUMN {
-                    cursor_column =NB_COLUMN-1;
-                }
-                if cursor_column<0 {
-                    cursor_column =0;
-                }
-            }
-            if let Some(button) = e.press_args() {
-                if button == Button::Mouse(MouseButton::Left) {
-                    let mut count = NB_RAW-1 as i64;
-                    while count>=0{
-                        match grid[cursor_column as usize][count as usize] {
-                            Token::NONE => {grid[cursor_column as usize][count as usize] = current_player;
-                                match current_player {
-                                    //send to socket
-                                    Token::RED => {
-                                        if check_winner(grid){
-                                            game_finish = true;
-                                            the_winner = Token::RED;
-                                        }
-                                        //current_player = Token::YELLOW; 
-                                        write!(stream, "jeton rouge\n");
-                                    },
-                                    Token::YELLOW => {
-                                        if check_winner(grid){
-                                            game_finish = true;
-                                            the_winner = Token::YELLOW;
-                                        }
-                                        //current_player = Token::RED;
-                                        write!(stream, "jeton jaune\n");
-                                    },
-                                    Token::NONE => {}
-                                    
-                                }
-                                break;
-                            }
-                            Token::YELLOW | Token::RED => {count-=1}
-                        }
-                    }                    
-
-                }
+        if !game_finish{  
+            game_finish = true;
+            let input : char = 'a'; 
+            let column_played = match input {
+                'a' => {0},
+                'z' => {1},
+                'e' => {2},
+                'r' => {3},
+                't' => {4},
+                'y' => {5},
+                'u' => {6},
+                _ => {-1}
             };
-            //efface la preview si la parti est fini
-            if game_finish {
-                current_player=Token::NONE}
-        }
-        if let Some(button) = e.press_args() {
-            use piston_window::Button::Keyboard;
-
-            if button == Keyboard(Key::R) {
-                grid = [[Token::NONE;NB_RAW as usize];NB_COLUMN as usize];
-                println!("Reset grid");
-                if game_finish{
-                    current_player = Token::YELLOW;
-                    the_winner = Token::NONE;
-                    game_finish = false
-                }
+            if column_played != -1 {             
+                grid = add_token(grid,column_played,current_player);
+                the_winner = check_winner(grid);
+            }                
+            match the_winner {
+                Token::RED | Token::YELLOW => {current_player=Token::NONE}
+                Token::NONE => {}
             }
         }
+        // if let Some(button) = e.press_args() {
+        //     use piston_window::Button::Keyboard;
+
+        //     if button == Keyboard(Key::R) {
+        //         grid = [[Token::NONE;NB_RAW as usize];NB_COLUMN as usize];
+        //         println!("Reset grid");
+        //         if game_finish{
+        //             current_player = Token::YELLOW;
+        //             the_winner = Token::NONE;
+        //             game_finish = false
+        //         }
+        //     }
+        // }
         window.draw_2d(&e, |c, g, _| {
-            clear([1.0; 4], g);
-
-                
-            let size_jeton= [margin_x + size_tile /10.0 + size_tile * cursor_column as f64, size_tile /10.0, size_tile * 8.0/10.0, size_tile * 8.0/10.0];
-            let next_jeton_preview;
-            match current_player {
-                Token::NONE => {next_jeton_preview = color_white},
-                Token::YELLOW => {next_jeton_preview = color_yellow},
-                Token::RED => {next_jeton_preview = color_red}
-            }
-            ellipse(next_jeton_preview, size_jeton, c.transform, g);
-            
+            clear([1.0; 4], g);            
 
             let outline_frame = math::margin_rectangle([0.0, 0.0, size_tile*NB_COLUMN as f64 + margin_x*2.0, size_tile*NB_RAW as f64+ margin_y*2.0], 0.0);
             let inline_frame = math::margin_rectangle([margin_x/2.0, margin_y/2.0, size_tile*NB_COLUMN as f64 + margin_x, size_tile*NB_RAW as f64 + margin_y], 0.0);
@@ -199,7 +158,19 @@ fn main() {
         }
     }
 
-    fn check_winner(grid:[[Token;NB_RAW as usize];NB_COLUMN as usize]) ->bool{
+    fn add_token( mut grid:[[Token;NB_RAW as usize];NB_COLUMN as usize], column: i64, current_player: Token) -> 
+        [[Token;NB_RAW as usize];NB_COLUMN as usize] {
+            let mut count = NB_RAW-1 as i64;
+            while count>=0{
+                match grid[column as usize][count as usize] {
+                    Token::NONE => {grid[column as usize][count as usize] = current_player;                    }
+                    Token::YELLOW | Token::RED => {count-=1}
+                }
+            }
+            return grid;
+        }
+        
+    fn check_winner(grid:[[Token;NB_RAW as usize];NB_COLUMN as usize]) ->Token{
 
         //verif columns
         let mut count_yellow;
@@ -213,9 +184,11 @@ fn main() {
                     Token::YELLOW => {count_red = 0; count_yellow +=1},
                     Token::RED => {count_yellow = 0; count_red +=1}
                 }
-                if  count_red>=4 || count_yellow>=4 {
-                    return true
-                };
+                if  count_red>=4 {
+                    return Token::RED
+                } else if count_yellow>=4{
+                    return Token::YELLOW
+                }
                 
             }
         } 
@@ -230,9 +203,11 @@ fn main() {
                     Token::YELLOW => {count_red = 0; count_yellow +=1},
                     Token::RED => {count_yellow = 0; count_red +=1}
                 }
-                if  count_red>=4 || count_yellow>=4 {
-                    return true
-                };
+                if  count_red>=4 {
+                    return Token::RED
+                } else if count_yellow>=4{
+                    return Token::YELLOW
+                }
             }
         }   
 
@@ -249,9 +224,11 @@ fn main() {
                         Token::YELLOW => {count_red = 0; count_yellow +=1},
                         Token::RED => {count_yellow = 0; count_red +=1}
                     }
-                    if  count_red>=4 || count_yellow>=4 {
-                        return true
-                    };
+                    if  count_red>=4 {
+                        return Token::RED
+                    } else if count_yellow>=4{
+                        return Token::YELLOW
+                    }
                 }
             }
         }  
@@ -268,12 +245,14 @@ fn main() {
                         Token::YELLOW => {count_red = 0; count_yellow +=1},
                         Token::RED => {count_yellow = 0; count_red +=1}
                     }
-                    if count_red>=4 || count_yellow>=4 {
-                        return true
+                    if  count_red>=4 {
+                        return Token::RED
+                    } else if count_yellow>=4{
+                        return Token::YELLOW
                     }
                 }
             }
         }  
-        return false
+        return Token::NONE
     }
 }
